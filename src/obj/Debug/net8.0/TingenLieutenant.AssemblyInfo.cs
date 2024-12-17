55dc9eb7301e427e91960ada6786410d792eb716
Chris Banwarth <github@aprettycoolprogram.com>
2024-12-17 11:27:11 -0500

    u241217

--- src/obj/Debug/net8.0/TingenLieutenant.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/TingenLieutenant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TingenLieutenant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4079922fe6548398272d1072b1ae3ad833aab67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6fa5c46676a4dd8edd42b9d97e2e6572d3ece5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TingenLieutenant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TingenLieutenant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
