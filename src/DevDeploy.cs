b36ff4764fbc52769fbeb32337145572dcfae088
Chris Banwarth <github@aprettycoolprogram.com>
2025-05-19 16:16:37 -0400

    u250519

---
+++ src/DevDeploy.cs
@@ -0,0 +1,24 @@
+﻿/* ████████ ██ ███    ██  ██████  ███████ ███    ██                                                                 
+ *    ██    ██ ██ ██  ██ ██   ███ █████   ██ ██  ██                                                              
+ *    ██    ██ ██   ████  ██████  ███████ ██   ████                                  
+ *                                                                                                                                                             
+ * ██      ██ ███████ ██    ██ ████████ ███████ ███    ██  █████  ███    ██ ████████ 
+ * ██      ██ █████   ██    ██    ██    █████   ██ ██  ██ ███████ ██ ██  ██    ██       
+ * ███████ ██ ███████  ██████     ██    ███████ ██   ████ ██   ██ ██   ████    ██    
+ *                 Tingen                                                                              
+                                                                                  
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tingen_Lieutenant
+{
+    internal class DevDeploy
+    {
+    }
+}
